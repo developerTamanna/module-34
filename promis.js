d6273012d886b277328e80552053b639c6be3d0c
developerTamanna <astamanna57@gmail.com>
2025-03-11 15:11:46 +0600

    promis return

---
+++ promis.js
@@ -0,0 +1,53 @@
+// const loadData =()=>{
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//     // .then(res =>console.log (res.json()))
+//     .then((res) => res.json() )
+//     .then((data) => {
+//         console.log(data)
+     
+//     })
+//     .catch((err) =>{
+//         console.log('this line :', err)
+//     } );
+    
+// }
+
+// loadData()
+
+
+
+// const fetchData =()=>{
+//     return new Promise((resolve,reject)=>{
+//     const status = true;
+//     if(status){
+//         resolve({name:"tamanna"})
+//     }
+//     else{
+//         reject("server error")
+//     }
+// })
+// }
+
+// fetchData()
+// .then((res) => console.log(res))
+// .catch((err) => console.log(err))
+
+
+
+
+const fetchData =()=>{
+     return new Promise((resolve, reject) => {
+         const status = true;
+         if(status){
+            resolve({name:"tamanna", age: 43})
+         } 
+         else{
+            reject("server error")
+         }
+      })
+}
+fetchData()
+.then((res) =>console.log(res) )
+.catch((err) => {
+    console.log(err)
+})
