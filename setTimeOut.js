75cc57dac22a9a20ff6740d6e5a7616260746f36
developerTamanna <astamanna57@gmail.com>
2025-03-11 12:37:39 +0600

    promis and promis all node

---
+++ setTimeOut.js
@@ -0,0 +1,14 @@
+
+//? set time out
+// console.log('start');
+// setTimeout(() => {
+//     console.log('this is delayed by 2 second');
+// }, 2000);
+// console.log('End')
+
+//? clear timeout
+
+// let timeOutID = setTimeout(()=>{
+//     console.log('this is not be shown')
+// }, 3000);
+// clearTimeout(timeOutID);
